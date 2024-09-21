87e902027a180d5c9f7c27134a51645acdada118
Kazem Arjmand <kaarj71@gmail.com>
2024-09-21 11:21:19 +0330

    feat(error-handler): implement error handler

---
+++ src/error-handler.ts
@@ -0,0 +1,8 @@
+import { ErrorHandler } from "./types";
+
+// Error handler middleware
+export const errorHandler: ErrorHandler = (err, req, res, next) => {
+  console.error(err);
+  res.end(err.message);
+  return;
+};
